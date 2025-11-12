0c4a12135a1662732c56dd69c06e358f488661c0
hufsjlee <deepsj1012@naver.com>
2025-11-12 21:14:40 +0900

    Initail commit

---
+++ src/main/java/com/flownote/flownote/FlowNoteApplication.java
@@ -0,0 +1,13 @@
+package com.flownote.flownote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/flownote/flownote/FlowNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flownote.flownote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
